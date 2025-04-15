c7dcef829a1bc2337b4f194b8a9963744d1490d9
srihari-290899 <rssrihari1999@gmail.com>
2025-04-15 22:26:20 +0530

    modifirf WF

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), eslint()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    envirnment: 'jsdom',
     setupFiles: './src/test/setup.js',
   },
 });
