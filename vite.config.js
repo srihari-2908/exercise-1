ffc7f3311d8122453ae330e8ac46c5552dd74176
srihari-290899 <rssrihari1999@gmail.com>
2025-04-15 22:29:48 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), eslint()],
   test: {
     globals: true,
-    envirnment: 'jsdom',
+    environment: 'jsdom',
     setupFiles: './src/test/setup.js',
   },
 });
